ddcc8e153e05163303f7ddacbafa1609d1ea0183
fanshun <15153019111@163.com>
2023-06-15 17:19:20 +0800

    patch: 补充依赖及相关配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
 import node_resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import del from 'rollup-plugin-delete';
 import terser from '@rollup/plugin-terser';
+import alias from '@rollup/plugin-alias';
+import del from 'rollup-plugin-delete';
 
 const common_plugins = [
     node_resolve(),
     commonjs(),
     typescript({
         tsconfig: './tsconfig.json',
+    }),
+    alias({
+        entries: [
+            {
+                find: '@tile_lnglat_transform',
+                replacement: 'src/plugins/tile-lnglat-transform/index.js',
+            },
+        ],
     }),
     del({
         targets: ['lib/**/*'],
@@ -24,14 +33,21 @@ export default {
             format: 'umd',
             name: 'SobekVHSC',
             entryFileNames: '[name].umd.min.js',
+            globals: {
+                konva: 'Konva',
+            },
         },
         {
             file: 'lib/main.esm.min.js',
             format: 'esm',
             entryFileNames: '[name].esm.min.js',
+            globals: {
+                konva: 'Konva',
+            },
         },
     ],
     plugins: [
         ...common_plugins,
     ],
+    external: ['konva'],
 }
